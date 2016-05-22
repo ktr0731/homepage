0b7ec96f4ea32c23f3e7594f97e2cb7cb0472642
Taro Aoki <aizu.s1230022@gmail.com>
2016-05-22 09:57:23 +0900

    [Modify] Use json for display contents

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
+var fs          = require('fs');
+var plumber     = require('gulp-plumber');
+var ejs         = require('gulp-ejs');
 
 gulp.task('default', function() {
   browserSync({
@@ -10,5 +13,16 @@ gulp.task('default', function() {
 
   gulp.watch('www/**/**', function() {
     browserSync.reload();
+  });
+
+  gulp.watch(['www_dev/index.html', 'www_dev/index.json'], function (e) {
+    if (e.type != 'deleted') {
+      var json = JSON.parse(fs.readFileSync('www_dev/index.json'));
+
+      gulp.src('www_dev/index.html')
+          .pipe(plumber())
+          .pipe(ejs(json))
+          .pipe(gulp.dest('www'));
+    }
   });
 });
