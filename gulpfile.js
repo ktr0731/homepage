ec04f29ed5e8ec7df8ef48dcd29de1cf72868a86
Taro Aoki <aizu.s1230022@gmail.com>
2017-03-04 12:03:17 +0900

    [MOD] gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var fs          = require('fs');
-var plumber     = require('gulp-plumber');
-var ejs         = require('gulp-ejs');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const fs          = require('fs');
+const plumber     = require('gulp-plumber');
+const ejs         = require('gulp-ejs');
 
-gulp.task('default', function() {
+gulp.task('default', () => {
   browserSync({
     server : {
       baseDir : 'www'
     }
   });
 
-  gulp.watch('www/**/**', function() {
+  gulp.watch('www/**/**', () => {
     browserSync.reload();
   });
 
-  gulp.watch(['www_dev/*.html', 'www_dev/index.json'], function (e) {
-    if (e.type != 'deleted') {
-      var json = JSON.parse(fs.readFileSync('www_dev/index.json'));
-
-      gulp.src('www_dev/index.html')
-          .pipe(plumber())
-          .pipe(ejs(json))
-          .pipe(gulp.dest('www'));
+  gulp.watch(['www_dev/*.html', 'www_dev/index.json'], e => {
+    if (e.type == 'deleted') {
+      return;
     }
-  });
 
+    const json = JSON.parse(fs.readFileSync('www_dev/index.json'));
+
+    gulp.src('www_dev/index.html')
+        .pipe(plumber())
+        .pipe(ejs(json))
+        .pipe(gulp.dest('www'));
+  });
 });
