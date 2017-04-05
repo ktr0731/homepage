8b95583bd428679bd60db905af99bf47f42a38db
lycoris0731 <aizu.s1230022@gmail.com>
2017-04-05 23:20:58 +0900

    [ADD] Speakerdeck link

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('default', () => {
     gulp.src('dest/index.html')
       .pipe(plumber())
       .pipe(ejs(json))
-      .pipe(gulp.dest('www'));
+      .pipe(gulp.dest('dest'));
   });
 });
 
