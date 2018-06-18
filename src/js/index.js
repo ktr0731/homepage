8a886bec944d0a31c7ee4655db2d46c3c2879013
ktr0731 <aizu.s1230022@gmail.com>
2018-06-18 14:41:34 +0900

    refactored

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,11 @@
-const f = () => {
-    fetch('/img/bg.png')
-        .then(res => res.arrayBuffer())
-        .then(buf => {
-            let binary = '';
-            var bytes = [].slice.call(new Uint8Array(buf));
-            bytes.forEach((b) => binary += String.fromCharCode(b));
-            const b64 = window.btoa(binary);
+fetch('/img/bg.png')
+    .then(res => res.arrayBuffer())
+    .then(buf => {
+        const bytes = [].slice.call(new Uint8Array(buf));
+        const b64 = window.btoa(bytes.reduce((a, c) => a += String.fromCharCode(c), ''));
 
-            const e = document.querySelector("body");
-            e.style["background-image"] = `url('data:image/png;base64,${b64}')`;
-            e.classList.remove("is-loading");
-            document.querySelector("main").classList.remove("is-loading");
-            document.querySelector("#profile").classList.remove("is-loading");
-        });
-}
+        const e = document.querySelector("body");
+        e.style["background-image"] = `url('data:image/png;base64,${b64}')`;
 
-window.setTimeout(f, 1000);
+        document.querySelectorAll(".is-loading").forEach(e => e.classList.remove("is-loading"));
+    });
