abcfeb4c525bfc56f87fdde167315fcd5b1e8cba
ktr0731 <aizu.s1230022@gmail.com>
2018-06-18 14:02:28 +0900

    use transition

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const f = () => {
+    fetch('/img/bg.png')
+        .then(res => res.arrayBuffer())
+        .then(buf => {
+            let binary = '';
+            var bytes = [].slice.call(new Uint8Array(buf));
+            bytes.forEach((b) => binary += String.fromCharCode(b));
+            const b64 = window.btoa(binary);
+
+            const e = document.querySelector("body");
+            e.style["background-image"] = `url('data:image/png;base64,${b64}')`;
+            e.classList.remove("is-loading");
+            document.querySelector("main").classList.remove("is-loading");
+        });
+}
+
+window.setTimeout(f, 1000);
